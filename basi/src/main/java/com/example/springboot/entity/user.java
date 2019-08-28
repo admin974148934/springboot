7b0920c0f01c709b24df608832e9771e344ff1c6
luyijian <974148934@qq.com>
2019-08-28 23:56:18 +0800

    springboot整合mysql

---
+++ basi/src/main/java/com/example/springboot/entity/user.java
@@ -0,0 +1,50 @@
+package com.example.springboot.entity;
+
+public class user {
+
+    private Long id;
+    private String name;
+    private String password;
+
+    public user() {
+    }
+
+    public user(Long id, String name, String password) {
+        this.id = id;
+        this.name = name;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    @Override
+    public String toString() {
+        return "user{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", password='" + password + '\'' +
+                '}';
+    }
+}
